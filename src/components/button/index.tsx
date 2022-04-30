d4697b25838c1a4e97b3eab362e46f0e12ffdc7d
thefjrm <felipemartinsplayernet@gmail.com>
2022-04-30 20:55:26 -0300

    Final app

--- src/components/button/index.tsx
+++
@@ -1,5 +0,0 @@
-import style from "./Button.module.scss";
-
-export default function Button(props: any) {
-  return <button className={style.botao}>{props.children}</button>;
-}
--- src/components/cronometro/index.tsx
+++ src/components/cronometro/index.tsx
@@ -1,15 +1,41 @@
-import Button from "../button";
+import Botao from "../Botao";
+import Relogio from "./relogio";
 import style from "./Cronometro.module.scss";
-import Relogio from "./relogio/index";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
 
-export default function Cronometro() {
+interface Props {
+  selecionado: ITarefa | undefined;
+  finalizarTarefa: () => void;
+}
+
+export default function Cronometro({ selecionado, finalizarTarefa }: Props) {
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
+
+  function regressiva(contador: number = 0) {
+    setTimeout(() => {
+      if (contador > 0) {
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+      }
+      finalizarTarefa();
+    }, 1000);
+  }
+
   return (
     <div className={style.cronometro}>
-      <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
       <div className={style.relogioWrapper}>
-        <Relogio />
+        <Relogio tempo={tempo} />
       </div>
-      <Button>Começar</Button>
+      <Botao onClick={() => regressiva(tempo)}>Começar!</Botao>
     </div>
   );
 }
